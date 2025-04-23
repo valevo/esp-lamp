7a08c3341fa8a4064afb1a9bc45ac52f5a5cc20f
valevo <valevogelmann@gmail.com>
2025-04-23 02:29:15 +0200

    primary versionnnnn DONNE

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
 #include <FastLED.h>
+
+#include <algorithm>
 
 // Replace with your network credentials
 const char *ssid = "Vale";
 const char *password = "13061993";
 
 const int ESP_BUILTIN_LED = 2;
-#define NUM_LEDS 10
-#define DATA_PIN D2
+#define NUM_LEDS 144
+#define DATA_PIN 5 // this is D5 (on the board/diagram)
+#define BRIGHTNESS 20
 
 CRGB leds[NUM_LEDS];
+
+bool gReverseDirection = false;
+void Fire2012();
 
 void setup()
 {
@@ -56,24 +63,132 @@ void setup()
 
   pinMode(ESP_BUILTIN_LED, OUTPUT);
 
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop()
 {
+  digitalWrite(ESP_BUILTIN_LED, HIGH);
+
   if (WiFi.isConnected())
   {
     ArduinoOTA.handle();
   }
 
-  digitalWrite(ESP_BUILTIN_LED, LOW);
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  delay(500);
+  // for (int i = 0; i < NUM_LEDS; i++)
+  // {
+  //   // digitalWrite(ESP_BUILTIN_LED, LOW);
+  //   leds[i] = CRGB::Green;
+  //   // delay(500);
+  // }
+  // FastLED.show();
+  // delay(2000);
 
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  digitalWrite(ESP_BUILTIN_LED, HIGH);
-  FastLED.show();
-  delay(500);
+  // for (int i = 0; i < NUM_LEDS; i++)
+  // {
+  //   // Now turn the LED off, then pause
+  //   // digitalWrite(ESP_BUILTIN_LED, HIGH);
+  //   leds[i] = CRGB::Red;
+  //   // delay(500);
+  // }
+  // FastLED.show();
+  // delay(2000);
+
+  Fire2012(); // run simulation frame
+
+  FastLED.show(); // display this frame
+  FastLED.delay(1000 / 30);
+
+  // CRGB colors[] = {CRGB::Blue, CRGB::Green, CRGB::Red, CRGB::Yellow, CRGB::Orange, CRGB::White, CRGB::Black};
+
+  // for (int i = 0; i < sizeof(colors) / sizeof(colors[i]); i++)
+  // {
+  //   for (int j = 0; j < NUM_LEDS; j++)
+  //   {
+  //     leds[j] = colors[i];
+  //   }
+  //   FastLED.show(); // display this frame
+  //   FastLED.delay(1000);
+  // }
+}
+
+// Fire2012 by Mark Kriegsman, July 2012
+// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
+////
+// This basic one-dimensional 'fire' simulation works roughly as follows:
+// There's a underlying array of 'heat' cells, that model the temperature
+// at each point along the line.  Every cycle through the simulation,
+// four steps are performed:
+//  1) All cells cool down a little bit, losing heat to the air
+//  2) The heat from each cell drifts 'up' and diffuses a little
+//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
+//  4) The heat from each cell is rendered as a color into the leds array
+//     The heat-to-color mapping uses a black-body radiation approximation.
+//
+// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
+//
+// This simulation scales it self a bit depending on NUM_LEDS; it should look
+// "OK" on anywhere from 20 to 100 LEDs without too much tweaking.
+//
+// I recommend running this simulation at anywhere from 30-100 frames per second,
+// meaning an interframe delay of about 10-35 milliseconds.
+//
+// Looks best on a high-density LED setup (60+ pixels/meter).
+//
+//
+// There are two main parameters you can play with to control the look and
+// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
+// in step 3 above).
+//
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 50, suggested range 20-100
+#define COOLING 35
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 180
+
+void Fire2012()
+{
+  // Array of temperature readings at each simulation cell
+  static uint8_t heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for (int k = NUM_LEDS - 1; k >= 2; k--)
+  {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  }
+
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if (random8() < SPARKING)
+  {
+    int y = random8(7);
+    heat[y] = qadd8(heat[y], random8(160, 255));
+    heat[y] = std::min((uint8_t)200, std::max((uint8_t)180, heat[y]));
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for (int j = 0; j < NUM_LEDS; j++)
+  {
+    CRGB color = HeatColor(heat[j]);
+    int pixelnumber;
+    if (gReverseDirection)
+    {
+      pixelnumber = (NUM_LEDS - 1) - j;
+    }
+    else
+    {
+      pixelnumber = j;
+    }
+    leds[pixelnumber] = color;
+  }
 }
